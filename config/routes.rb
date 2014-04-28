2b22419f209b0130cbec5f97c603ef5786e8119a
Raghu Betina <git@rugb.com>
2014-04-28 16:12:38 -0500

    Rails 4.1.0 update, removed turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-FortuneTeller::Application.routes.draw do
+Rails.application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
   get("/zodiacs/:sign", { :controller => "zodiacs", :action => "show" })
